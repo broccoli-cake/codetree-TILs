318cbf3c28311558edf6a527ff917c62a782c493
broccoli cake <gchk0905@gmail.com>
2024-08-07 00:12:44 +0900

    Title: 달에서 무게 구하기, Time: 119ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240807/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int w1 = 13;
+        double w2 = 0.165;
+
+        System.out.printf("%d * %.6f = %.6f", w1, w2, (w1 * w2));
+    }
+}
